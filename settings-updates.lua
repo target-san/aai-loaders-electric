7dbfd7fc043319cd27f603984fb837cae8203f4f
Target-san <target.san@gmail.com>
2023-05-21 15:13:06 +0300

    Force aai-loaders into expensive mode by overriding mode setting

---
+++ settings-updates.lua
@@ -0,0 +1,8 @@
+﻿-- Patch aai-loaders' mode to be always 'expensive'
+-- This is needed to disable aai-loaders' scripted fluid supply
+-- and patch them to use electricity
+local loaders_mode = data.raw["string-setting"]["aai-loaders-mode"]
+
+loaders_mode.hidden = true
+loaders_mode.allowed_values = {"expensive"}
+loaders_mode.default_value = "expensive"
